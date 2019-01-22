e9a0fd0fe040e86d3436a6ef2f5d296d0ec633d4
Mindstormer <louisemileploix@icloud.com>
2019-01-22 10:51:48 +0000

    Update world.js

--- iSurvive/world.js
+++ iSurvive/world.js
@@ -65,7 +65,7 @@ class World {
         this.sun = this.createPlanet(sun, 200);
         this.moon = this.createPlanet(moon, 100);
 
-        this.tick = 20000;
+        this.tick = 25000;
         this.speed = 10000;
 
         this.loadRadius = 5;
